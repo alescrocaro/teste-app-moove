7ecdaf7e436dd0b6a6d901fb6e9a97866a3f7363
alescrocaro <alescrocaro@gmail.com>
2022-08-18 10:26:16 -0300

    feat: lista dos 3 ultimos filmes lancados

---
+++ src/components/moviesInTheatres/index.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+import { api } from '../../services/api';
+
+export default function MoviesInTheatres() {
+  const [movies, setMovies] = useState([]);
+
+  const getData = async () => {
+    let data = await api.get(
+      '/movie/now_playing?api_key=0b52f543bf49e978de68ec261123f4f2&language=pt-BR'
+    );
+
+    data = data.data.results;
+
+    data.splice(3, 17);
+    // console.log(data);
+    setMovies(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="moviesContainer">
+      <p className="moviesText">Últimos 3 filmes em cartaz!</p>
+      <div className="cardsContainer">
+        {movies.map(movie => (
+          <Link to={`movie/${movie.id}`} key={movie.id} className="movieCard">
+            <img
+              src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+              alt="Poster"
+              className="posterImg"
+            />
+            <p className="movieName">{movie.title}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
