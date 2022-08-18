359cb8aee35d024634aace2ec9f42377381b1192
alescrocaro <alescrocaro@gmail.com>
2022-08-18 10:27:55 -0300

    feat: pagina inicial

---
+++ src/pages/Home/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './style.css';
+
+import Header from '../../components/header';
+import Search from '../../components/search';
+import MoviesInTheatres from '../../components/moviesInTheatres';
+
+export default function Home() {
+  const [pageYPosition, setPageYPosition] = useState(0);
+
+  function getPageYAfterScroll() {
+    setPageYPosition(window.scrollY);
+  }
+  window.addEventListener('scroll', getPageYAfterScroll);
+
+  return (
+    <>
+      {pageYPosition > 100 && (
+        <a id="backToTop" href="#home">
+          Voltar ao topo
+        </a>
+      )}
+      <Header />
+      <Search />
+      <MoviesInTheatres />
+    </>
+  );
+}
