e755606daf370c167bd3cb185f22484ae4b22038
alescrocaro <alescrocaro@gmail.com>
2022-08-18 10:27:13 -0300

    feat: pesquisa de filmes pelo nome
    se nada estiver sendo pesquisado, mostra os filmes mais populares do momento

---
+++ src/components/search/index.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+import { api } from '../../services/api';
+
+export default function Search() {
+  const [searchText, setSearchText] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const getData = async () => {
+    let data = {};
+    if (searchText === '') {
+      data = await api.get(
+        'movie/popular?api_key=0b52f543bf49e978de68ec261123f4f2&language=pt-BR'
+      );
+      console.log('nada');
+    } else {
+      data = await api.get(
+        `/search/movie?api_key=0b52f543bf49e978de68ec261123f4f2&query="${searchText}"&language=pt-BR`
+      );
+      console.log('pesquisou ', searchText);
+    }
+    data = data.data.results;
+
+    data.splice(3, 17);
+    console.log(data);
+    setMovies(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [searchText]);
+
+  return (
+    <div className="mainContainer">
+      <p className="mainText">Procure pelo filme desejado</p>
+      <div className="searchBoxContainer">
+        <input
+          type="text"
+          className="searchBox"
+          placeholder="Insira o nome do filme"
+          onChange={e => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button className="searchIcon">Buscar</button>
+      </div>
+      {!searchText && (
+        <p className="tempText">mostrando filmes mais populares...</p>
+      )}
+      <div className="cardsContainer">
+        {movies.map(movie => (
+          <Link to={`movie/${movie.id}`} key={movie.id} className="movieCard">
+            <img
+              src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+              alt="Poster"
+              className="posterImg"
+            />
+            <p className="movieName">{movie.title}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
