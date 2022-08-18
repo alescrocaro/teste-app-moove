f7bdff2c324c9fab010e7165d14f042b4d967eea
alescrocaro <alescrocaro@gmail.com>
2022-08-18 10:30:35 -0300

    feat: rotas para páginas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+import NotFound from './pages/NotFound/index.js';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/movie/:id" element={<Movie />} />
+        <Route exact path="/*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
